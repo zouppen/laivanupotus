71a3a826e37a131d49b2330ccf36b60df601ef3d
Joel Lehtonen <joel.lehtonen@iki.fi>
2020-12-16 12:13:24 +0100

    Reorganize test to a bit more readable list

--- src/Tests.hs
+++ src/Tests.hs
@@ -5,27 +5,28 @@ import Test.HUnit
 import Boat
 
 -- Boat creation tests
-boatPairs = [
-  ( Boat { boatX = 0
-         , boatY = 0
-         , boatLength = 1
-         , boatOrientation = Horizontal
-         } ,
-    [ (0,0) ]
-  ), ( Boat { boatX = 5
-            , boatY = 3
-            , boatLength = 4
-            , boatOrientation = Horizontal
-            } ,
-       [ (5,3), (6,3), (7,3), (8,3) ]
-  ), ( Boat { boatX = 4
-            , boatY = 1
-            , boatLength = 3
-            , boatOrientation = Vertical
-            } ,
-       [ (4,1), (4,2), (4,3) ]
-     )
-  ]
+boat1 = Boat { boatX = 0
+             , boatY = 0
+             , boatLength = 1
+             , boatOrientation = Horizontal
+             }
+
+boatH = Boat { boatX = 5
+             , boatY = 3
+             , boatLength = 4
+             , boatOrientation = Horizontal
+             }
+
+boatV = Boat { boatX = 4
+             , boatY = 1
+             , boatLength = 3
+             , boatOrientation = Vertical
+             }
+
+boatPairs = [(boat1, [(0,0)])
+            ,(boatH, [(5,3), (6,3), (7,3), (8,3)])
+            ,(boatV, [(4,1), (4,2), (4,3)])
+            ]
 
 testBoatCreation = TestList $ map test boatPairs
   where test (boat,reference) = TestCase $ assertEqual (show boat) (freeformBoat reference) (renderBoat boat)
