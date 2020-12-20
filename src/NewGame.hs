f9d73e3cf2df26dccbbdfbfcfacb000039edf518
Joel Lehtonen <joel.lehtonen@iki.fi>
2020-12-21 00:14:00 +0100

    Add an action which creates a valid boat

---
+++ src/NewGame.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE RecordWildCards #-}
+-- |Create new game
+module NewGame where
+
+import Control.Monad.Except
+import Control.Monad.State.Lazy
+import Control.Monad.Loops (iterateUntil)
+import System.Random
+
+import Types
+import Engine
+
+-- random-1.2 is not yet very widespread so implementing states with old version
+
+-- |Generate random ship which fits on the board
+randomBoat :: RandomGen s => Board -> Int -> State s Boat
+randomBoat Board{..} boatLength = do
+  boatOrientation <- (\h -> if h then Horizontal else Vertical) <$> state random
+  let (highX, highY) = case boatOrientation of
+                         Horizontal -> (maxX-boatLength+1, maxY)
+                         Vertical   -> (maxX, maxY-boatLength+1)
+  boatX <- state $ randomR (minX,highX)
+  boatY <- state $ randomR (minY,highY)
+  pure Boat{..}
+
+-- |Generate ship of given length until it fits
+fitShip :: RandomGen s => Rules -> [Boat] -> Int -> State s Boat
+fitShip rules boats boatLength = iterateUntil valid $ randomBoat (board rules) boatLength
+  where valid boat = case runExcept $ createGame rules (boat:boats) of
+                       Left OutOfBounds   -> error "Generated boat out of bounds. Fatal."
+                       Left CountMismatch -> True  -- Do not stress about the count at this point.
+                       Right _            -> True  -- Game generated, this is also fine.
+                       _                  -> False -- Otherwise let's try again
